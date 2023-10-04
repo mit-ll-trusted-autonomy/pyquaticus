34fb47e28fe7f3040f8f0800944dd39d3b0e890b
makaimann <makaim@cs.stanford.edu>
2023-10-03 20:32:34 -0400

    Add Jervis Bay Configuration (#29)

--- test/pyquaticus_moos_bridge_rand_test.py
+++ test/pyquaticus_moos_bridge_rand_test.py
@@ -1,18 +1,20 @@
 import time
 
 from pyquaticus.moos.pyquaticus_moos_bridge import PyQuaticusMoosBridge
-from pyquaticus.moos.config import WestPointConfig
+from pyquaticus.moos.config import WestPointConfig, JervisBayConfig
 
 if __name__ == "__main__":
-    watcher = PyQuaticusMoosBridge("localhost", "red_one", 9011,
-                      [], ["blue_one"], moos_config=WestPointConfig(), 
-                      quiet=False)
+    # NOTE must pick the corresponding configuration object based on which mission you're running
+    # and must choose the correct teammates/opponents and timewarp
+    env = PyQuaticusMoosBridge("localhost", "red_one", 9011,
+                      [], ["blue_one"], moos_config=WestPointConfig(),
+                      timewarp=4, quiet=False)
 
     try:
-        obs = watcher.reset()
-        action_space = watcher.action_space
+        obs = env.reset()
+        action_space = env.action_space
         for i in range(800):
-            obs, _, _, _, _ = watcher.step(action_space.sample())
+            obs, _, _, _, _ = env.step(action_space.sample())
         print("Finished loop")
     finally:
-        watcher.close()
+        env.close()
