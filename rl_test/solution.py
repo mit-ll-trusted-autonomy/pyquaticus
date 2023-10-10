a03c3dbf831dad442caf69f583fa727f5a27e7cd
John Kliem <78662188+john-kliem@users.noreply.github.com>
2023-10-10 09:20:10 -0400

    Updated competition system to allow for heuristic submissions (previously removed) (Australian Request) (#34)
    Co-authored-by: john-kliem <john.kliem@nrl.navylmil>

--- rl_test/solution.py
+++ rl_test/solution.py
@@ -13,19 +13,17 @@ class solution:
 	
     def __init__(self):
 		#Load in policy or anything else you want to load/do here
+        #NOTE: You can only load from files that are in the same directory as the solution.py or a subdirectory
         
-        #Load in learned policies
+        #Load in learned policies see examples below:
         self.policy_one = Policy.from_checkpoint(os.path.dirname(os.path.realpath(__file__))+ '/checkpoint_000006/policies/agent-0-policy/')
         self.policy_two = Policy.from_checkpoint(os.path.dirname(os.path.realpath(__file__))+ '/checkpoint_000006/policies/agent-1-policy/')
 
 	#Given an observation return a valid action agent_id is agent that needs an action, observation space is the current normalized observation space for the specific agent
-    def compute_action(self,agent_id:int, observation:list):
-		#If making a heuristic based agent you might want the observation space unnormalized:
-		#observation = env.agent_obs_normalizer.unnormalized(observation)
-        #See how the base policies are constructed for more information on creating a heuristic based agent
+    def compute_action(self,agent_id:int, observation_normalized:list, observation:dict):
         if agent_id == 0 or agent_id == 2:
-            return self.policy_one.compute_single_action(observation)[0]
+            return self.policy_one.compute_single_action(observation_normalized)[0]
         else:
-            return self.policy_two.compute_single_action(observation)[0]
+            return self.policy_two.compute_single_action(observation_normalized)[0]
 
 #END OF CODE SECTION
