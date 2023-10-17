04e2cbb33155c4b0c2bdaefaf3e01f3418ee65f9
makaimann <makaim@cs.stanford.edu>
2023-10-16 23:54:13 -0400

    Search for region info file

--- pyquaticus/moos/get_field.sh
+++ pyquaticus/moos/get_field.sh
@@ -3,5 +3,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DEPS=$(realpath $SCRIPT_DIR/../../../)
 MISSION_DIR="$DEPS/moos-ivp-aquaticus/missions/jervis-2023"
 echo "Using Mission directory: $MISSION_DIR"
-grep -A 2 REGION $MISSION_DIR/surveyor/region_info_red_one.txt > field.txt
-grep -A 1 BLUE_FLAG $MISSION_DIR/surveyor/region_info_red_one.txt > flags.txt
+pattern="$MISSION_DIR/surveyor/region_info_*.txt"
+files=( $pattern )
+REGION_INFO="${files[0]}"
+echo "Using region info file: $REGION_INFO"
+grep -A 2 REGION $REGION_INFO > field.txt
+grep -A 1 BLUE_FLAG $REGION_INFO > flags.txt
