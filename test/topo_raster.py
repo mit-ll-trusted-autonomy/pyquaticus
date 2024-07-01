83e48f6c56ce30fb0103265fec6c1aedfa759ea7
Peter Crowley <petertc@bu.edu>
2024-07-01 17:05:28 -0400

    contour zoom out

--- test/topo_raster.py
+++ test/topo_raster.py
@@ -9,10 +9,11 @@ from geographiclib.geodesic import Geodesic
 from math import ceil
 from PIL import Image
 from scipy.ndimage import label
+from timeit import default_timer as timer
 
 
-COORD1 = (42.306671732900284, -71.0306793176816)
-COORD2 = (42.35077795666068, -70.94882396266765)
+COORD1 = (42.3066717, -71.0306793)
+COORD2 = (42.3507779, -70.9488239)
 
 def crop_tiles(img, ext, w, s, e, n, ll=True):
     """
@@ -72,7 +73,7 @@ if __name__ == "__main__":
     print()
 
     img, ext = cx.bounds2img(
-        w=COORD1[1], s=COORD1[0], e=COORD2[1], n=COORD2[0], zoom="auto", source=source, ll=True,
+        w=COORD1[1], s=COORD1[0], e=COORD2[1], n=COORD2[0], zoom=12, source=source, ll=True,
         wait=0, max_retries=2, n_connections=1, use_cache=False, zoom_adjust=None
     )
     print("Initial image size:", img[:,:,:-1].shape)
@@ -90,7 +91,7 @@ if __name__ == "__main__":
     # cv2.waitKey(0)
 
     # test distance between mercator and gps distance (3 miles)
-    water = (42.32418681048927, -71.02230485803544)
+    water = (42.3241868, -71.0223048)
     water_x, water_y  = mt.xy(water[1], water[0])
 
     left, bottom, right, top = ext
@@ -107,61 +108,77 @@ if __name__ == "__main__":
     )
     labeled_mask, _ = label(mask, structure=water_connectivity)
     target_label = labeled_mask[water_pixel_y, water_pixel_x]
-    water_mask = (labeled_mask == target_label) + (38 <= gray_img) *  (gray_img <= 40)
+    water_mask = (labeled_mask == target_label) + (38 <= gray_img) * (gray_img <= 40)
+    land_mask = np.logical_not(water_mask)
 
     water_image = Image.fromarray(water_mask)
     water_image.save("water_mask.png")
 
     water_binary = 255*water_mask.astype(np.uint8)
+    land_binary = 255*land_mask.astype(np.uint8)
 
     #contours (https://docs.opencv.org/2.4/modules/imgproc/doc/structural_analysis_and_shape_descriptors.html?#findcontours)
-    water_contours, _ = cv2.findContours(water_binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    land_contours_old, _ = cv2.findContours(land_binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     water_color_img = cv2.cvtColor(water_binary, cv2.COLOR_GRAY2BGR)
+    land_color_img = cv2.cvtColor(land_binary, cv2.COLOR_GRAY2BGR)
 
-    water_contours_img = cv2.drawContours(copy.deepcopy(water_color_img), water_contours, -1, (0,255,0), 3)
+    water_contours_img = cv2.drawContours(copy.deepcopy(water_color_img), land_contours_old, -1, (0,255,0), 3)
+    land_contours_img = cv2.drawContours(copy.deepcopy(land_color_img), land_contours_old, -1, (0,255,0), 3)
 
     cv2.imwrite("water_contours_old.png", water_contours_img)
 
     #remove holes inside land (water) with water contours
-    water_contours_img_gray = cv2.cvtColor(water_contours_img, cv2.COLOR_BGR2GRAY)
-    water_pixel_color_gray = water_contours_img_gray[water_pixel_y, water_pixel_x]
-    gray_mask = water_contours_img_gray == water_pixel_color_gray
-    labeled_mask_gray, _ = label(gray_mask, structure=water_connectivity)
+    land_contours_img_gray = cv2.cvtColor(land_contours_img, cv2.COLOR_BGR2GRAY)
+    water_pixel_color_gray = land_contours_img_gray[water_pixel_y, water_pixel_x]
+    gray_water_mask = land_contours_img_gray == water_pixel_color_gray
+    labeled_mask_gray, _ = label(gray_water_mask, structure=water_connectivity)
     target_label_gray = labeled_mask_gray[water_pixel_y, water_pixel_x]
-    water_mask_gray = (labeled_mask_gray == target_label_gray)
+    land_mask_new = np.logical_not(labeled_mask_gray == target_label_gray)
 
     #contours 2
-    water_binary_gray = 255*water_mask_gray.astype(np.uint8)
+    land_binary_new = 255*land_mask_new.astype(np.uint8)
 
-    water_contours_gray, _ = cv2.findContours(water_binary_gray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    land_contours_new, _ = cv2.findContours(land_binary_new, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    print("# water contours old:", len(water_contours))
-    print("# water contours new:", len(water_contours_gray))
+    print("# land contours old:", len(land_contours_old))
+    print("# land contours new:", len(land_contours_new))
 
-    water_contours_img_gray = cv2.drawContours(copy.deepcopy(water_color_img), water_contours_gray, -1, (0,255,0), 3)
+    land_contours_img_new = cv2.drawContours(copy.deepcopy(water_color_img), land_contours_new, -1, (0,255,0), 3)
 
-    cv2.imwrite("water_contours_new.png", water_contours_img_gray)
+    cv2.imwrite("water_contours_new.png", land_contours_img_new)
 
     #approximate contours
     epsilon = 0.001
-    water_cnts_approx = []
+    cnts_approx = []
 
-    for i, cnt in enumerate(water_contours_gray):
+    for i, cnt in enumerate(land_contours_new):
         eps = epsilon * cv2.arcLength(cnt,True)
-        water_cnts_approx.append(cv2.approxPolyDP(cnt, eps, True))
-        print(len(water_cnts_approx[-1]))
+        cnt_approx = cv2.approxPolyDP(cnt, eps, True)
+        cnts_approx.append(cnt_approx)
+        print(len(cnt), "->", len(cnt_approx), "vertices")
 
-    c = cv2.drawContours(copy.deepcopy(water_color_img), water_cnts_approx, -1, (0,255,0), 3)
+    c = cv2.drawContours(copy.deepcopy(water_color_img), cnts_approx, -1, (0,255,0), 3)
     cv2.imwrite("approx_water_contour.png", c)
 
-    poly_points = np.concatenate(water_cnts_approx[2], axis=0)
-    print(min(poly_points[:, 1])) 
+    poly_points = np.concatenate(cnts_approx[4], axis=0)
+    poly_points[:, 1] = -(poly_points[:, 1] - (c.shape[0] - 1))
+    # print(min(poly_points[:, 0]))
+    # print(max(poly_points[:, 0]))
+    # print(min(poly_points[:, 1])) 
+    # print(max(poly_points[:, 1]))
     s_poly = shapely.Polygon(poly_points)
-    print('here')
-
-    line = shapely.LineString([(400, 0), (400, 600)])
-    a = shapely.intersection(s_poly, line)
-    print(a)
+    line = shapely.LineString([(500, 0), (550, 600)])
+    
+    start = timer()
+    intersec = shapely.intersection(s_poly, line)
+    end = timer()
+    int_compute_time = end - start
+    n_rays = 16
+    n_agents = 6
+    worst_case_total_rc_compute = int_compute_time * len(cnts_approx) * n_rays * n_agents
+    print("Intersection compute time:", end - start)
+    print("Worse-case ray casting compute time:", worst_case_total_rc_compute)
+    print(intersec)
 
     # Geodesic.WGS84.Direct(lat1=34., lon1=148., azi1=90., s12=10_000.) #s12 is the distance from the first point to the second in meters
