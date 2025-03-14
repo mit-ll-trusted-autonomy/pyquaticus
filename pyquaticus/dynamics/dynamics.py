8e46798839a14e8bab4c51a0611da8e53c3a5b48
Peter Crowley <petertc@bu.edu>
2025-03-14 12:05:41 -0400

    fixing issue where speed was saved in mercator meters/s instead of meters/s

--- pyquaticus/dynamics/dynamics.py
+++ pyquaticus/dynamics/dynamics.py
@@ -433,8 +433,6 @@ class BaseUSV(Dynamics):
         """
         # Calculate average speed
         avg_speed = (new_speed + self.speed) / 2.0
-        if self.gps_env:
-            avg_speed /= self.meters_per_mercator_xy
 
         # Calculate average heading
         hdg_rad = np.deg2rad(self.heading)
@@ -449,8 +447,11 @@ class BaseUSV(Dynamics):
             [np.sin(avg_hdg), np.cos(avg_hdg)] #sine/cos swapped because of the heading / angle difference
         )
 
-        new_pos = self.pos + vel * self.dt
         new_speed = np.linalg.norm(vel)
+        if self.gps_env:
+            new_pos = self.pos + (vel / self.meters_per_mercator_xy) * self.dt
+        else:
+            new_pos = self.pos + vel * self.dt
 
         return new_pos, new_speed
 
