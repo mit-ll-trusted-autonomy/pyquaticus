3ec8f29ddc07ceb678c8f69c880f1d0025f22588
Peter Crowley <petertc@bu.edu>
2025-03-31 11:35:13 -0400

    setting up moos-ivp-rlagent in moos folder

---
+++ moos/moos-ivp-rlagent/src/pRLMonitor/RLMonitor_Info.h
@@ -0,0 +1,18 @@
+/****************************************************************/
+/*   NAME: Mike McCarrick                                       */
+/*   ORGN: NRL Code 5554                                        */
+/*   FILE: RLMonitor_Info.h                                     */
+/*   DATE: 23 Feb 2021                                          */
+/****************************************************************/
+
+#ifndef RLMonitor_INFO_HEADER
+#define RLMonitor_INFO_HEADER
+
+void showSynopsis();
+void showHelpAndExit();
+void showExampleConfigAndExit();
+void showInterfaceAndExit();
+void showReleaseInfoAndExit();
+
+#endif
+
