5de714fc57ca5141e1b31c9c39201d9d4fad3a99
ptcrowley <petertc@bu.edu>
2026-03-20 13:27:42 -0400

    temp save

--- pyquaticus/envs/pyquaticus.py
+++ pyquaticus/envs/pyquaticus.py
@@ -1058,7 +1058,7 @@ class PyQuaticusEnv(PyQuaticusEnvBase):
             )
 
         # Previous state
-        self.prev_state = copy.deepcopy(self.state)
+        # self.prev_state = copy.deepcopy(self.state)
 
         # Process incoming actions
         action_dict = {}
@@ -2852,6 +2852,7 @@ class PyQuaticusEnv(PyQuaticusEnvBase):
         #agents
         new_pos_by_env = new_pos[:, None, :]
         agent_dists = np.linalg.norm(new_pos_by_env - agent_poses, axis=-1)
+        print(new_pos, agent_poses.shape)
         radii = np.tile(self.agent_radius, (self.agent_radius.shape[0], 1))
         envs_with_agent_collision = np.any(agent_dists <= self.agent_radius[agent_idx] + radii, axis=-1)
         valid_pos[envs_with_agent_collision] = False
